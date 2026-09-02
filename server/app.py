bb73190b1b7caa2e6901d5d2701e9aaf49e0ff0f
saahiyo <saahiyo@users.noreply.huggingface.co>
2026-09-02 17:45:54 +0530

    Fix web server import and add TgCrypto

--- server/app.py
+++ server/app.py
@@ -5,9 +5,14 @@ from server.routes import setup_routes
 logger = logging.getLogger(__name__)
 
 
-async def create_web_server() -> web.AppRunner:
+async def create_web_app() -> web.Application:
     app = web.Application(client_max_size=1024 * 1024 * 10)  # 10 MB request body limit for headers
     setup_routes(app)
+    return app
+
+
+async def create_web_server() -> web.AppRunner:
+    app = await create_web_app()
     runner = web.AppRunner(app)
     await runner.setup()
     return runner
